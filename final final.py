777005320e456904ad0e01c0c38ea1b2c2722c73
aylalafleur <ayla.lafleur@temple.edu>
2024-12-09 13:36:25 -0500

    Add files via upload

---
+++ final final.py
@@ -0,0 +1,49 @@
+from PIL import Image
+import math
+
+
+
+def pixel(file):
+    image = Image.open(file)
+    size = width, height = image.size
+    num = int(input("How many pixels do you want? "))
+    num = 1/num
+    pix = int(math.sqrt(((width*height)*num)))
+    right = low = pix
+    left = up = 0
+   
+    for i in range(int(height/pix)+1):
+        for i in range(int(width/pix)+1):
+            average = average_color(image, (left,up,right,low))
+            image.paste(average, (left,up,right,low)) 
+            right += pix
+            
+            left += pix
+           
+        left = 0
+        right = pix
+        up += pix
+        low += pix
+
+    image.show()
+
+
+    
+def average_color (image, box):
+    cropimg = image.crop(box)
+    R = 0
+    G = 0
+    B = 0
+    data = list(cropimg.getdata())
+    for color in data:
+        r, g, b = color
+        R += r/len(data)
+        B += b/len(data)
+        G += g/len(data)
+    RGB = (int(R), int(G), int(B))
+    return RGB
+
+
+
+pixel("flower.jpg")
+    
